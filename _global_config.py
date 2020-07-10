585652c5163cb0b94bb1e71f24f384c57968585c
YilunAllenChen <allenchen@gatech.edu>
2020-07-10 08:51:13 -0400

    coda page added

--- _global_config.py
+++ _global_config.py
@@ -1,5 +1,4 @@
 
-DB_HOST = "mongodb+srv://trading_agent:Lc2gwI5aUjnVXYoU@cluster0.y7n4w.mongodb.net/Dowwin?retryWrites=true&w=majority"
 BOT_THRESHOLD = 20000
 ELIMINATION_THRESHOLD = 60000
 STARTING_FUND = 100000
