91cd0e61baaae32f88e4ca36a9a111d2b03a1ed6
YilunAllenChen <allenchen@gatech.edu>
2020-08-23 16:14:03 -0400

    upload log updated

--- Analysis_tools.py
+++ Analysis_tools.py
@@ -11,14 +11,16 @@ Examples
 '''
 
 bots = client['Dowwin']['tradebots']
-bots = [item for item in bots.find({}, {'_id':0, 'id': 1, 'chars': 1})]
+bots = [item for item in bots.find({}, {'_id':0, 'id': 1, 'nextUpdate': 1, 'value': 1})]
 
 
+fig, axs = plot.subplots(2,2)
+fig.suptitle('Dowwin Analysis Toolkit Dashboard')
 
+bins_value = [100 * i + 60000 for i in range(700)]
+bins_nextUpdate = [timedelta(hours=1) * i + datetime.now() for i in range (144)]
 
-bins = [100 * i + 60000 for i in range(700)]
-bins = [timedelta(hours=1) * i + datetime.now() for i in range (144)]
-bins = [i for i in range(200)]
-plot.hist([bot.get('chars').get('operatinginterval') for bot in bots], bins=bins)
+axs[0][0].hist([bot.get('value') for bot in bots], bins=bins_value)
+
+axs[0][1].hist([bot.get('nextUpdate') for bot in bots], bins=bins_nextUpdate)
 plot.show()
-
