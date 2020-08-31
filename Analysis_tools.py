34ee221bf6aaef9f4bfcf542218de53d5e5e0f18
YilunAllenChen <allenchen@gatech.edu>
2020-08-31 19:25:09 -0400

    to csv functionality added

--- Analysis_tools.py
+++ Analysis_tools.py
@@ -1,5 +1,6 @@
 
 import sys
+import numpy
 sys.path.extend(['tasks', 'adapters_apis', 'static_data', 'utility_modules'])
 
 
@@ -11,12 +12,19 @@ from pprint import pformat
 from datetime import datetime, timedelta
 
 
-log("Fetching market data...")
-all_stock_data = sync_get_market_image_minimal()
-log("Data fetched. Processing...")
-market = {}
-for item in all_stock_data:
-    market[item.get('Symb')] = item.get('Info')
+# Output to py
+# log("Fetching market data...")
+# all_stock_data = sync_get_market_image_minimal()
+# log("Data fetched. Processing...")
+# market = {}
+# for item in all_stock_data:
+#     market[item.get('Symb')] = item.get('Info')
 
-with open("market_data.py",'w') as f:
-    f.writelines(pformat(market))
+# with open("market_data.py",'w') as f:
+#     f.writelines(pformat(market))
+
+# Output to csv
+# import pandas as pd
+# df = pd.DataFrame(market).transpose()
+# print(df)
+# df.to_excel('output_data.xls')
